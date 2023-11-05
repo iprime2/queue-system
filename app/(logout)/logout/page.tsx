1d520d80b98a839c856352da0861f6b58fb2d550
Sushil Gupta <kalwarsushil2@gmail.com>
2023-11-05 17:30:41 +0530

    logout refactor

--- app/(logout)/logout/page.tsx
+++ app/(logout)/logout/page.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 
 const LogoutPage = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-
-  if (!session) {
-    router.push("/");
-  }
-
   useEffect(() => {
     const handleSignOut = async () => {
       await signOut({ callbackUrl: "/" });
@@ -20,7 +12,11 @@ const LogoutPage = () => {
     handleSignOut();
   }, []);
 
-  return <h1>Logging Out!</h1>;
+  if (typeof window !== "undefined") {
+    return <h1>Logging Out!</h1>;
+  }
+
+  return null;
 };
 
 export default LogoutPage;
