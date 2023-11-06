d4ca9eb95a71978da0f914ca4245b68dcc9914d7
Sushil Gupta <kalwarsushil2@gmail.com>
2023-11-06 13:55:47 +0530

    routes protection all inside admin

--- middleware.ts
+++ middleware.ts
@@ -2,29 +2,18 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  const protectedPaths = [
-    "/admin/dashboard",
-    "/admin/users",
-    "/admin/departments",
-    "/admin/tokens",
-    "/admin/counters",
-  ];
-  const isPathProtected = protectedPaths?.some((path) => pathname == path);
-  const res = NextResponse.next();
-  if (isPathProtected) {
+  const protectedPaths = ["/admin", "/settings", "/profile"];
+
+  if (protectedPaths.some((path) => pathname.startsWith(path))) {
     const token = await getToken({
       req,
       secret: process.env.NEXTAUTH_JWT_SECRET,
     });
-
-    // console.log("Middleware");
-    // console.log(token);
-
     if (!token) {
       const url = new URL(`/`, req.url);
       url.searchParams.set("callbackUrl", encodeURI(pathname));
       return NextResponse.redirect(url);
     }
   }
-  return res;
+  return NextResponse.next();
 }
