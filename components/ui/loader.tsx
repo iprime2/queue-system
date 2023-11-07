79ee2f18958b74b5fff835ebf811a5843b147e19
Sushil Gupta <kalwarsushil2@gmail.com>
2023-11-07 16:28:20 +0530

    loader refactor

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,7 +1,11 @@
-'use client'
+"use client";
 
-import { ClipLoader } from 'react-spinners'
+import { ClipLoader } from "react-spinners";
 
 export const Loader = () => {
-  return <ClipLoader color='#3498db' size={50} />
-}
+  return (
+    <div className="flex h-view w-full items-center justify-center">
+      <ClipLoader color="#3498db" size={50} />
+    </div>
+  );
+};
